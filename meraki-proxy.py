3ee6ccaccb329d9ee803d6af674609926a52667b
esoberanc <56157480+esoberanc@users.noreply.github.com>
2025-05-02 16:06:53 +0200

    Update meraki-proxy.py

--- meraki-proxy.py
+++ meraki-proxy.py
@@ -528,7 +528,8 @@ def enviar_informe():
 @app.route("/api/consumo-diario")
 def consumo_diario():
     try:
-        df = obtener_datos_sheets()
+        df = obtener_datos_consumo()
+
 
         if df.empty:
             return jsonify([])
