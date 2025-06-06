db47adcd8558a12f44728b8ed3a0d98034700971
esoberanc <56157480+esoberanc@users.noreply.github.com>
2025-06-06 22:13:52 +0200

    Update meraki-proxy.py

--- meraki-proxy.py
+++ meraki-proxy.py
@@ -704,15 +704,17 @@ def iniciar_monitoreo_automatico():
 if __name__ == "__main__":
     iniciar_monitoreo_automatico()
 
+    def tarea_enviar_informe():
+        with app.app_context():
+            enviar_informe()
+
    # Programar envío automático
     scheduler = BackgroundScheduler()
     scheduler.add_job(tarea_enviar_informe, "interval", minutes=1)
     scheduler.start()
     
   #  scheduler.add_job(envio_automatico_informe, "cron", day_of_week="mon", hour=8, minute=0)
-    def tarea_enviar_informe():
-        with app.app_context():
-            enviar_informe()
+   
 
     
     print("✅ Scheduler iniciado correctamente")
